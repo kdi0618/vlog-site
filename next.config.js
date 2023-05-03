97cb60155c64dda6e1a31a9997b8978b654c0aae
Kodai Kudo <k.kodai0618@gmail.com>
2023-05-03 20:04:45 +0900

    mod: fix configurations

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   experimental: {
     appDir: true,
-    output: 'export',
-    distDir: 'dist',
-    trailingSlash: true,
   },
 }
 
