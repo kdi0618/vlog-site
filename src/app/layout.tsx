8f30fd7a9a07c4d644db03b3f6b275162cf1db1a
Kodai Kudo <k.kodai0618@gmail.com>
2023-07-08 17:57:04 +0900

    fix(GA4): reattach the GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './global.css'
 import { Inter } from 'next/font/google'
+import { GoogleTagManager, GoogleTagManagerId } from './components/GoogleTagManager';
+import { googleTagManagerId } from './utils/gtm';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <GoogleTagManager
+          googleTagManagerId={googleTagManagerId as GoogleTagManagerId}
+        />
+      </body>
     </html>
   )
 }
